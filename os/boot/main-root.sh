3cc8e6f8130729adb6d3f3484eb60fd12d776654
Gustavo A <gustavo.atencio@outlook.com>
2024-01-06 15:09:15 -0500

    Separe scripts for root and users

---
+++ os/boot/main-root.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+dir=$(readlink -f "$(dirname "$0")")
+scripts_dir="$dir/scripts/root"
+
+# Run all scripts in the scripts dir
+
+scripts=$(ls $scripts_dir | xargs -I _ echo "$scripts_dir/_")
+
+for script in $scripts; do
+	sh $script &
+done
--- os/boot/main-user.sh
+++ os/boot/main-user.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 dir=$(readlink -f "$(dirname "$0")")
-scripts_dir="$dir/scripts"
+scripts_dir="$dir/scripts/user"
 
 # Run all scripts in the scripts dir
 
 scripts=$(ls $scripts_dir | xargs -I _ echo "$scripts_dir/_")
 
 for script in $scripts; do
-	sh $script
+	sh $script &
 done
