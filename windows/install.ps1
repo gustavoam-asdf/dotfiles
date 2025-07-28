f782cea4cf4b09aa2004f28e71974b5281a7f490
Gustavo A. <gustavo.atencio.mauricio@outlook.com>
2025-07-28 17:18:09 -0500

    add environment variable to to bin dir

--- windows/install.ps1
+++ windows/install.ps1
@@ -3,11 +3,19 @@ Set-StrictMode -Version Latest
 sudo config --enable normal
 
 if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
+	exit $LASTEXITCODE
 }
 
 sudo winget import ./programs/installation/winget.json
 
 $bin_dir = "$PSScriptRoot/bin"
+New-Item $bin_dir -ItemType Directory -Force > $null
+$bin_dir = Resolve-Path -Path $bin_dir
 
 pwsh $PSScriptRoot/programs/installation/unlisted.ps1 --install_dir $bin_dir
+
+$user_path = [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+if (-not ($user_path -like "*$bin_dir*")) {
+	[System.Environment]::SetEnvironmentVariable("Path", $user_path + "$bin_dir;", "User")
+}
