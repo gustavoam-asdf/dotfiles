c8fb4b485eac7af5652a6fea69d7dfac688252fc
Gustavo A. <gustavo.atencio.mauricio@outlook.com>
2025-07-28 14:22:07 -0500

    add installation of android platform tools

--- windows/programs/installation/unlisted.ps1
+++ windows/programs/installation/unlisted.ps1
@@ -1,1 +1,32 @@
-Invoke-WebRequest -Uri "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" -OutFile "./bin/yt-dlp.exe"
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$install_dir
+)
+
+if (-not (Test-Path $install_dir)) {
+    New-Item $install_dir -ItemType Directory -Force > $null
+}
+
+
+$tmp_dir = "./tmp"
+New-Item $tmp_dir -ItemType Directory -Force > $null
+
+$full_install_dir = Resolve-Path $install_dir
+
+Write-Host "Installing in $full_install_dir"
+
+$yt_dlp_url = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
+Invoke-WebRequest -Uri $yt_dlp_url -OutFile "$full_install_dir/yt-dlp.exe"
+Write-Host "✅ Installed yt-dlp"
+
+$platform_tools_url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip" 
+$platform_tools_zip = "$tmp_dir/platform-tools.tmp.zip"
+Invoke-WebRequest -Uri $platform_tools_url -OutFile $platform_tools_zip
+Expand-Archive -LiteralPath $platform_tools_zip -DestinationPath "$tmp_dir"
+Move-Item -Path "$tmp_dir/platform-tools/*" -Destination $full_install_dir
+Remove-Item $platform_tools_zip
+Write-Host "✅ Installed platform tools"
+
+
+Remove-Item $tmp_dir -Force -Recurse
