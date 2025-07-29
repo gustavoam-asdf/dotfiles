48f9c6dff49c76ecd6e6637567c18b869afef1a1
Gustavo A. <gustavo.atencio.mauricio@outlook.com>
2025-07-29 08:57:20 -0500

    install fonts

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -6,7 +6,7 @@ if ($LASTEXITCODE -ne 0) {
 	exit $LASTEXITCODE
 }
 
-sudo winget import ./programs/installation/winget.json
+sudo winget import $PSScriptRoot/installation/winget.json
 
 # Read fresh environment variables after installation
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path")
@@ -15,7 +15,7 @@ $bin_dir = "$PSScriptRoot/bin"
 New-Item $bin_dir -ItemType Directory -Force > $null
 $bin_dir = Resolve-Path -Path $bin_dir
 
-pwsh $PSScriptRoot/programs/installation/unlisted.ps1 --install_dir $bin_dir
+pwsh $PSScriptRoot/installation/unlisted.ps1 --install_dir $bin_dir
 
 $user_path = [System.Environment]::GetEnvironmentVariable("Path", "User")
 
@@ -23,3 +23,5 @@ if (-not ($user_path -like "*$bin_dir*")) {
 	[System.Environment]::SetEnvironmentVariable("Path", $user_path + "$bin_dir;", "User")
 	$env:Path += "$bin_dir;"
 }
+
+pwsh $PSScriptRoot/installation/fonts.ps1
