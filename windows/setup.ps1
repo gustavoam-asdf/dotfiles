bb3d30c98d7c0f7c29194e728d1b8651a8c503b0
Gustavo A. <gustavo.atencio@outlook.com>
2025-08-25 16:00:04 -0500

    fix: remove redundant command to enable Windows Subsystem for Linux

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -7,7 +7,6 @@ if ($LASTEXITCODE -ne 0) {
 	exit $LASTEXITCODE
 }
 
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All
 wsl --install
 
 $dotFilesDir = Resolve-Path $PSScriptRoot
